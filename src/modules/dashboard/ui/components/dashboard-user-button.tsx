20487c3c7f1312b749939440314fb714d23ed7eb
farukulWD <farukgb1999@gmail.com>
2025-06-06 10:12:58 +0600

    Make dashboard navbar, responsive and also search dialog

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,5 +1,15 @@
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,6 +18,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { authClient } from "@/lib/auth-client";
 
 import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
@@ -17,10 +28,12 @@ import React from "react";
 function DashboardUserButton() {
   const { data, isPending } = authClient.useSession();
   const router = useRouter();
+  const isMobile = useIsMobile();
 
   if (isPending || !data?.user) {
     return null;
   }
+
   const handleLogout = async () => {
     authClient.signOut({
       fetchOptions: {
@@ -30,6 +43,43 @@ function DashboardUserButton() {
       },
     });
   };
+
+  if (isMobile) {
+    return (
+      <Drawer>
+        <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 gap-2 overflow-hidden cursor-pointer">
+          {data?.user?.image ? (
+            <Avatar>
+              <AvatarImage src={data?.user?.image} alt={data?.user?.name} />
+            </Avatar>
+          ) : (
+            <GeneratedAvatar variant="initials" seed={data?.user?.name} />
+          )}
+          <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1  min-w-0">
+            <p className="truncate text-sm w-full">{data?.user?.name}</p>
+            <p className="truncate text-xs w-full">{data?.user?.email}</p>
+          </div>
+          <ChevronDownIcon className="size-4 shrink-0" />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{data?.user?.name}</DrawerTitle>
+            <DrawerDescription>{data?.user?.email}</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button variant={"outline"} onClick={() => {}}>
+              <CreditCardIcon className="size-4" />
+              Billing
+            </Button>
+            <Button variant={"outline"} onClick={ handleLogout}>
+              <LogOutIcon className="size-4" />
+              Logout
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 gap-2 overflow-hidden cursor-pointer">
